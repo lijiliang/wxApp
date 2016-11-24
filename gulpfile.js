8f5adc3d0fc1c1ca15cb433a0bbc5508116ac26b
李继梁 <ljl@xxxx.com>
2016-11-24 15:58:08 +0800

    编译wxml文件

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,13 @@ const cssnano = require('cssnano');
 const atImport = require('postcss-import');
 const autoprefixer = require('autoprefixer');
 const base64 = require('gulp-base64');
+const rename = require('gulp-rename')
 
 // 源目录
 const srcDir = {
     image: './src/**/*.{png,jpg,jpeg,svg}',
     js: './src/**/*.js',
-    wxml: './src/**/*.wxml',
+    views: './src/**/*.{html,wxml}',
     wxss: './src/**/*.wxss',
     json: './src/**/*.json'
 }
@@ -55,17 +56,27 @@ gulp.task('js', ()=>{
 })
 gulp.task('js:watch', ()=>{
     watch([srcDir.js], {event: ['add','change','unlink']}, (file)=>{
-        gulp.src(srcDir.js)
-            .pipe(gulp.dest(distDir))
-            .on('end', ()=>{
-                console.log(file.path + ' complite!')
-            });
+        js(file)
     })
 })
 
 gulp.task('wxss', ()=>{
     compileWxss(srcDir.wxss, distDir); // 编译 .wxss
 });
+gulp.task('wxss:watch', ()=>{
+    watch([srcDir.wxss], {event: ['add','change','unlink']}, (file)=>{
+        compileWxss(srcDir.wxss, distDir, file); // 编译 .wxss
+    })
+})
+
+gulp.task('views', ()=>{
+    views();
+})
+gulp.task('views:watch', ()=>{
+    watch([srcDir.views], {event: ['add','change','unlink']}, (file)=>{
+        views(file);
+    })
+})
 
 gulp.task('clean', ()=>{
     del([
@@ -75,12 +86,12 @@ gulp.task('clean', ()=>{
 
 // 开发环境下编译
 gulp.task('dev', ()=>{
-    gulp.start('image','image:watch','json','json:watch','js','js:watch', 'wxss')
+    gulp.start('image','image:watch','json','json:watch','js','js:watch', 'wxss','wxss:watch','views','views:watch')
 });
 
 // 正式环境下编译
 gulp.task('build', ['clean'], ()=>{
-    gulp.start('image','json')
+    // gulp.start('image','json')
 });
 
 // 编译image文件
@@ -98,20 +109,43 @@ function json() {
     gulp.src(srcDir.json)
         .pipe(gulp.dest(distDir))
         .on('end', ()=>{
-            // console.log('json complite!')
+            console.log('json complite!')
         });
 }
 
 // 编译js
-function js(){
+function js(file){
     gulp.src(srcDir.js)
         .pipe(gulp.dest(distDir))
         .on('end', ()=>{
-            console.log('js complite!')
+            if(file){
+                console.log(file.path + ' complite!')
+            }else{
+                console.log('js complite!')
+            }
         });
 }
 
-function compileWxss(src, dist) {
+// 编译html、wxml文件成wxml
+function views(file){
+    gulp.src(srcDir.views)
+        .pipe(rename({extname: '.wxml'}))
+        .pipe(gulp.dest(distDir))
+        .on('end', ()=>{
+            if(file){
+                console.log(file.path + ' complite!');
+            }else{
+                console.log('wxml complite! ');
+            }
+        });
+}
+/**
+ * [compileWxss 编译WXSS文件]
+ * @param  {[string]} src  [源目录]
+ * @param  {[string]} dist [输出目录]
+ * @param  {[string]} file [当前文件名，非必填]
+ */
+function compileWxss(src, dist, file) {
 	return gulp.src(src)
 	// .pipe(base64({
 	// 	extensions: ['png', /\.jpg#datauri$/i],
@@ -119,4 +153,11 @@ function compileWxss(src, dist) {
 	// }))
 	.pipe(postcss(processes))
 	.pipe(gulp.dest(dist))
+    .on('end', ()=>{
+        if(file){
+            console.log(file.path + ' complite!')
+        }else{
+            console.log('wxss complite!')
+        }
+    })
 }
